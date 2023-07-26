dc0674d5b76fbe21100dc32e1cfe4779adc82b37
José Bastos <josebezerrabastos@gmail.com>
2023-07-26 00:18:21 -0300

    Deixando o HTML dinâmico através do typescript

---
+++ src/app/components/card-box/card-box.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDataModel';
+
+@Component({
+  selector: 'app-card-box',
+  templateUrl: './card-box.component.html',
+  styleUrls: ['./card-box.component.css']
+})
+export class CardBoxComponent {
+  accountData: AccountDataModel = {
+   
+      name: "Rodrigo Goes",
+      account: {
+          agency: "0000",
+          number: "00.000000-0",
+      },
+      card: {
+          limit: 0,
+          number: "0000"
+      }
+  
+  };
+
+}
+
